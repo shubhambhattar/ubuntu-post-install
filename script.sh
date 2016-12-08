d53c8c3153290bee7d512237ad0304059d56a61c
Shubham Bhattar <shubhambhattar@users.noreply.github.com>
2016-12-08 13:54:14 +0530

    Stored output of all the commands in a log file for future reference.

--- script.sh
+++ script.sh
@@ -1,43 +1,43 @@
 #!/usr/bin/env bash
 
 # add repositories, update and upgrade
-sudo add-apt-repository -y ppa:webupd8team/atom
-sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-sudo apt-add-repository -y ppa:webupd8team/java
-sudo add-apt-repository -y ppa:slgobinath/safeeyes
-sudo apt update
-sudo apt upgrade
+sudo add-apt-repository -y ppa:webupd8team/atom | tee output.log
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3 | tee -a output.log
+sudo apt-add-repository -y ppa:webupd8team/java | tee -a output.log
+sudo add-apt-repository -y ppa:slgobinath/safeeyes | tee -a output.log
+sudo apt update | tee -a output.log
+sudo apt upgrade | tee -a output.log
 
 # install the application of PPA
 
 sudo apt -y install \
     atom sublime-text-installer idle vim g++ safeeyes \
     redshift sqlitebrowser weechat vlc chromium-browser \
-    gparted filezilla
+    gparted filezilla | tee -a output.log
 
 # install java
-sudo apt install oracle-java8-installer
+sudo apt install oracle-java8-installer | tee -a output.log
 
 # kill bluetooth on startup and start redshift
-sudo sed -i -e '$i rfkill block bluetooth \nredshift \n' /etc/rc.local
+sudo sed -i -e '$i rfkill block bluetooth \nredshift \n' /etc/rc.local | tee -a output.log
 
 # sublime-text-3 - configurations, snippets and packages
-mkdir -p ~/.config/sublime-text-3/Installed\ Packages/
-mkdir -p ~/.config/sublime-text-3/Packages/User/
+mkdir -p ~/.config/sublime-text-3/Installed\ Packages/ | tee -a output.log
+mkdir -p ~/.config/sublime-text-3/Packages/User/ | tee -a output.log
 
 cp data/st3/Installed-Packages/Package\ Control.sublime-package \
-    ~/.config/sublime-text-3/Installed\ Packages/
-cp -ar data/st3/Packages/User/* ~/.config/sublime-text-3/Packages/User
+    ~/.config/sublime-text-3/Installed\ Packages/ | tee -a output.log
+cp -ar data/st3/Packages/User/* ~/.config/sublime-text-3/Packages/User | tee -a output.log
 
 # install atom packages
-apm install autocomplete-python
-apm install advanced-open-file
-apm install atom-runner
-apm install gpp-compiler
+apm install autocomplete-python | tee -a output.log
+apm install advanced-open-file | tee -a output.log
+apm install atom-runner | tee -a output.log
+apm install gpp-compiler | tee -a output.log
 
 # configuration and snippets in atom
-cp data/atom/* ~/.atom/
+cp data/atom/* ~/.atom/ | tee -a output.log
 
 # vim configuration file
-shopt -s dotglob
-cp -ar ./data/dotfiles/* ~
+shopt -s dotglob | tee -a output.log
+cp -ar ./data/dotfiles/* ~ | tee -a output.log
