ed86c7c1a1c93b563407d5dce79b84cc3f19dfa3
Shubham Bhattar <shubhambhattar@gmail.com>
2016-12-10 02:21:28 +0530

    kvm commands added

--- script.sh
+++ script.sh
@@ -18,6 +18,12 @@ sudo apt -y install \
 # install java
 sudo apt install oracle-java8-installer | tee -a output.log
 
+if [ "$egrep -c '(svm|vmx)' /proc/cpuinfo" > 0 ]; then
+    sudo apt -y install qemu-kvm libvirt-bin \
+        bridge-utils virt-manager | tee -a output.log
+    sudo adduser shubham libvirtd | tee -a output.log
+fi
+
 # kill bluetooth on startup and start redshift
 sudo sed -i -e '$i rfkill block bluetooth \nredshift \n' /etc/rc.local | tee -a output.log
 
