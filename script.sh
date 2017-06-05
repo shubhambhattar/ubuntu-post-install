09f0df33bde925102f4585d78ac1e96e75fda08e
Shubham Bhattar <shubhambhattar@users.noreply.github.com>
2017-06-05 20:42:01 +0530

    Added commands for Shotwell [Image Viewer].

--- script.sh
+++ script.sh
@@ -5,6 +5,7 @@ sudo add-apt-repository -y ppa:webupd8team/atom | tee output.log
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-3 | tee -a output.log
 sudo apt-add-repository -y ppa:webupd8team/java | tee -a output.log
 sudo add-apt-repository -y ppa:slgobinath/safeeyes | tee -a output.log
+sudo add-apt-repository -y ppa:yg-jensge/shotwell
 sudo add-apt-repository -y ppa:varlesh-l/indicator-kdeconnect | tee -a output.log
 sudo add-apt-repository -y ppa:gezakovacs | tee -a output.log
 sudo apt -y update | tee -a output.log
@@ -15,7 +16,7 @@ sudo apt -y upgrade | tee -a output.log
 sudo apt -y install \
     atom sublime-text-installer idle vim g++ safeeyes \
     redshift sqlitebrowser weechat vlc chromium-browser \
-    gparted filezilla kdeconnect unetbootin unrar | tee -a output.log
+    gparted filezilla kdeconnect unetbootin unrar shotwell| tee -a output.log
 
 # install java
 sudo apt install oracle-java8-installer | tee -a output.log
